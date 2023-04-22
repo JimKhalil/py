b0c6d64ef2769b00c77c068bc51fc6c6325998ca
Abiansyah19 <21410100029@dinamika.ac.id>
2023-04-23 01:12:06 +0700

    hello

--- text2.py
+++ text2.py
@@ -2,8 +2,26 @@ import datetime as dt
 
 #format string
 
+#dapat menggunakan angka bilangan bulat
+
+"""
+:, untuk , di ordo bilangan ribuan
+:3f untuk mengambil 3 angka float dibelakang  koma 
+:d bilangan bulat
+:% untuk jadi persen
+:%2 untuk mengambil berapa angka di belakang titik
+"""
 nama = "abi"
 format_str = (f"muhammad haris maulana koliq {nama}")
+print(format_str)
+
+#operasi aritmatika didalam format string
+
+a=10000
+b=2
+
+format_str = (f"nilai {a*b:,}")
+
 print(format_str)
 
 #harus import untuk menggunakan fungsi date
