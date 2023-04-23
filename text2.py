791f23438c29fd48a9d5fdcdd5f671be50dc1bb3
Abiansyah19 <21410100029@dinamika.ac.id>
2023-04-23 09:23:51 +0700

    hello

--- text2.py
+++ text2.py
@@ -64,3 +64,15 @@ print(f"Tanggal lahir anda : {tanggal_lahir}")
 umur_hari = hari_ini - tanggal_lahir
 umur = umur_hari.days//365
 print(f"umur anda adalah : {umur} tahun")
+
+nama = input("siapa nama ? ")
+
+#harus satu baris if elsenya 
+#kondisi tidak perlu tanda kurung
+
+if nama=="abi" : 
+    print("Muhammad Haris Maualana Koliq Abiansyah")
+    print(f"ini {nama}")
+    print("belajar python")
+else : 
+    print(f"kamu {nama}")
