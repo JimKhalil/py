10fe7ebe47c68472e2ccf2d4a37972b0071fd06d
Abiansyah19 <21410100029@dinamika.ac.id>
2023-05-17 15:07:57 +0700

    hello

--- text3.py
+++ text3.py
@@ -6,5 +6,24 @@ if nama=="abi" :
     print("Muhammad Haris Maualana Koliq Abiansyah")
     print(f"ini {nama}")
     print("belajar python")
+#tidak ada else if adanya elif
+elif nama=="haris":
+    print("hello world")
 else : 
     print(f"kamu {nama}")
+
+kangka=[9,1,2,3,4]
+
+for i in kangka :
+    print(f"i sekarang => {i} = {kangka}")
+
+print("end program")
+
+# contoh while 
+a = 1
+
+while a<6 :
+    print(a)
+    # harus dikasih counter
+    # += itu menambahkan pada variablenya sendiri
+    a+=1
