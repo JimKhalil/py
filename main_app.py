3dbdcd9cb6883eefe379875dbb7618610f68e605
Abiansyah19 <21410100029@dinamika.ac.id>
2023-05-22 12:29:02 +0700

    success

---
+++ main_app.py
@@ -0,0 +1,24 @@
+#function
+
+def hello_world():
+        print("hello world")
+
+hello_world()
+
+# function dengan argument/parameter input``
+
+'''
+template
+def nama_fungsi():
+'''
+
+nama = input("siapakah nama anda ?")
+a = int(input("jumlah yang akan diulang ?"))
+i = 0
+def hello_world2(nama,i,a) :
+    while i < a :
+        i+=1
+        print(f"ini nama yang diulan {nama}",i)
+hello_world2(nama,i,a)
+
+# def function(angka1=input('input1'), angka2=input 2)
--- text3.py
+++ text3.py
@@ -6,7 +6,7 @@ if nama=="abi" :
     print("Muhammad Haris Maualana Koliq Abiansyah")
     print(f"ini {nama}")
     print("belajar python")
-<<<<<<< HEAD
+
 #tidak ada else if adanya elif
 elif nama=="haris":
     print("hello world")
@@ -21,14 +21,33 @@ for i in kangka :
 print("end program")
 
 # contoh while 
-a = 1
+a = 0
 
-while a<6 :
-    print(a)
+
+while a<4 :
+    a+=1
+    print("angka sekarang " , a)
+    if a == 4 :
+        print("nice")
+        #continue untuk menstop sementara while dengan kondisinya dan hasil continue mengembalikan ke while lagi
+        continue
     # harus dikasih counter
     # += itu menambahkan pada variablenya sendiri
+    print("ini")
+    
+a = 0
+while a<6 :
     a+=1
-=======
+    print("angka sekarang " , a)
+    if a == 4 :
+        print("nice")
+        #break untuk menghentikan while
+        break
+    # harus dikasih counter
+    # += itu menambahkan pada variablenya sendiri
+    print("hello")
+    
+
+
 else : 
     print(f"kamu {nama}")
->>>>>>> 2d247c5f7b4f3db891fe5763d08a35a6db324489
